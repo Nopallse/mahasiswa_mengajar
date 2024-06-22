c95e3406912d8ce32d9dad6ad14cca893a8a7c44
Ilham Nofaldi <81660180+Ilhamnof@users.noreply.github.com>
2024-06-22 13:40:50 +0700

    sudah bisa

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const controller = require("../controller/ubahpass");
-const { login, requireAuth,redirectIfAuthenticated,logout } = require('../middleware/auth');
+const { login,redirectIfAuthenticated,logout } = require('../middleware/auth');
 
 /* GET home page. */
 router.get('/',redirectIfAuthenticated, function(req, res, next) {
@@ -9,14 +8,7 @@ router.get('/',redirectIfAuthenticated, function(req, res, next) {
 });
 router.post('/login', login);
 router.post('/logout', logout);
-router.post("/ubahPassword", requireAuth, async (req, res) => {
-    try {
-        await controller.ubahPassword(req, res);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "server error" });
-    }
-    });
+
 router.get('/error', function(req, res, next) {
     res.render('error', { title: 'Transkrip Nilai' });
 });
